bb43f2b2ddcd3a326b7639d5d1930c49dcf0a4b0
duziem <frenzyokeke@gmail.com>
2022-03-10 21:24:42 +0000

    edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -7,16 +7,50 @@ import Articles from './components/Articles';
 const title = "Sorting Articles";
 
 function App({articles}) {
+    const [sortart, setSortart]= useState([])
+
+    const handleClick= (e)=>{
+        if(e.target.id.includes('upvoted')){
+            let art= [...articles];
+            art.sort((a,b)=>{
+                return (b.upvotes - a.upvotes);
+            })
+            setSortart(art)
+        }
+        if(e.target.id.includes('recent')){
+            let art= [...articles];
+            art.sort((a,b)=>{
+                if(b.date > a.date){
+                    return 1;
+                }
+                if(b.date < a.date){
+                    return -1;
+                }
+                return 0;
+
+            })
+            setSortart(art)
+        }
+        
+    }
+
+    useEffect(()=>{
+        let art= [...articles];
+            art.sort((a,b)=>{
+                return (b.upvotes - a.upvotes);
+            })
+        setSortart(art)
+    }, [])
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button id='upvoted' data-testid="most-upvoted-link" className="small" onClick={handleClick}>Most Upvoted</button>
+                <button id='recent' data-testid="most-recent-link" className="small" onClick={handleClick}>Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={sortart}/>
         </div>
     );
 
